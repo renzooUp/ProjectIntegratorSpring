5f1eed4245cc3f7aabec13a49b6d6d2da946a139
rubirengifo <106537961+rubirengifo@users.noreply.github.com>
2022-12-12 19:12:24 -0500

    x

--- src/main/java/com/upeu/demo/repository/UsuarioRepository.java
+++ src/main/java/com/upeu/demo/repository/UsuarioRepository.java
@@ -10,5 +10,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
-    
+        public Usuario findByUsername(String username);
 }
